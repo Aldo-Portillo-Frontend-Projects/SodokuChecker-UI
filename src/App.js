25d23bbaf0dc5f5ac0479ddb7e507d85e0bc5fed
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-12-03 01:02:35 -0600

    Create state and pass setState down

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import { sudokuIsValid } from "./algo/main";
 import Board from "./components/Board";
 
 function App() {
+  const [sodoku, setSodoku] = useState([
+    [ 8,9,5,   7,4,2,   1,3,6 ],
+    [ 2,7,1,   9,6,3,   4,8,5 ],
+    [ 4,6,3,   5,8,1,   7,9,2 ],
+
+    [ 9,3,4,   6,1,7,   2,5,8 ],
+    [ 5,1,7,   2,3,8,   9,6,4 ],
+    [ 6,8,2,   4,5,9,   3,7,1 ],
+
+    [ 1,5,9,   8,7,4,   6,2,3 ],
+    [ 7,4,6,   3,2,5,   8,1,9 ],
+    [ 3,2,8,   1,9,6,   5,4,7 ]])
+
   return (
     <div className="App">
       <h1>Sodoku Checker</h1>
-      <Board />
+      <Board setSodoku={setSodoku}/>
+      <h2>The Game is {sudokuIsValid(sodoku) ? "Complete" : "Incomplete"}</h2>
     </div>
   );
 }
