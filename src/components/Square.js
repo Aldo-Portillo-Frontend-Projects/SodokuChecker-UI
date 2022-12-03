6c0dd4aa34580ea6f0a0f7cfdb0fd1c578992ebe
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-12-02 20:33:49 -0600

    Remove number 0

--- src/components/Square.js
+++ src/components/Square.js
@@ -7,16 +7,17 @@ function Square() {
     <>
     <p>Current Number {number}</p>
     <div className='square'>
-        <p onClick={() => setNumber(0)}>0</p>
-        <p onClick={() => setNumber(1)}>1</p>
-        <p onClick={() => setNumber(2)}>2</p>
-        <p onClick={() => setNumber(3)}>3</p>
-        <p onClick={() => setNumber(4)}>4</p>
-        <p onClick={() => setNumber(5)}>5</p>
-        <p onClick={() => setNumber(6)}>6</p>
-        <p onClick={() => setNumber(7)}>7</p>
-        <p onClick={() => setNumber(8)}>8</p>
-        <p onClick={() => setNumber(9)}>9</p>
+        <div className='numbers'>
+            <p onClick={() => setNumber(1)}>1</p>
+            <p onClick={() => setNumber(2)}>2</p>
+            <p onClick={() => setNumber(3)}>3</p>
+            <p onClick={() => setNumber(4)}>4</p>
+            <p onClick={() => setNumber(5)}>5</p>
+            <p onClick={() => setNumber(6)}>6</p>
+            <p onClick={() => setNumber(7)}>7</p>
+            <p onClick={() => setNumber(8)}>8</p>
+            <p onClick={() => setNumber(9)}>9</p>
+        </div>
     </div>
     </>
   )
