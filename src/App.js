d68680d77ec57440143b77cd398b3bd3e28f26aa
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-12-02 20:24:47 -0600

    Render square component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Square from "./components/Square";
 
 function App() {
   return (
     <div className="App">
       <h1>Sodoku Checker</h1>
+      <Square />
     </div>
   );
 }
