156f0ec84a73024ceb0e85979980f60546435ca4
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-12-03 01:06:25 -0600

    Pass setSodoku props down

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,18 +1,18 @@
 import React from 'react'
 import Section from './Section'
 
-export default function Board() {
+export default function Board({setSodoku}) {
   return (
     <div className='board'>
-        <Section backgroundColor="blue"/>
-        <Section backgroundColor="white"/>
-        <Section backgroundColor="blue"/>
-        <Section backgroundColor="white"/>
-        <Section backgroundColor="blue"/>
-        <Section backgroundColor="white"/>
-        <Section backgroundColor="blue"/>
-        <Section backgroundColor="white"/>
-        <Section backgroundColor="blue"/>
+        <Section backgroundColor="blue" setSodoku={setSodoku}/>
+        <Section backgroundColor="white" setSodoku={setSodoku}/>
+        <Section backgroundColor="blue" setSodoku={setSodoku}/>
+        <Section backgroundColor="white" setSodoku={setSodoku}/>
+        <Section backgroundColor="blue" setSodoku={setSodoku}/>
+        <Section backgroundColor="white" setSodoku={setSodoku}/>
+        <Section backgroundColor="blue" setSodoku={setSodoku}/>
+        <Section backgroundColor="white" setSodoku={setSodoku}/>
+        <Section backgroundColor="blue" setSodoku={setSodoku}/>
     </div>
   )
 }
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,18 +1,18 @@
 import React from 'react'
 import Square from './Square'
 
-export default function Section({backgroundColor}) {
+export default function Section({backgroundColor, setSodoku}) {
   return (
     <div className={`section ${backgroundColor}`}>
-        <Square />
-        <Square />
-        <Square />
-        <Square />
-        <Square />
-        <Square />
-        <Square />
-        <Square />
-        <Square />
+        <Square setSodoku={setSodoku} />
+        <Square setSodoku={setSodoku} />
+        <Square setSodoku={setSodoku} />
+        <Square setSodoku={setSodoku} />
+        <Square setSodoku={setSodoku} />
+        <Square setSodoku={setSodoku} />
+        <Square setSodoku={setSodoku} />
+        <Square setSodoku={setSodoku} />
+        <Square setSodoku={setSodoku} />
     </div>
   )
 }
