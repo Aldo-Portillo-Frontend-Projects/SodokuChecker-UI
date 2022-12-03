a4e1dc718da9ea26e64dea78468dbbee9b867723
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-12-02 20:37:04 -0600

    Add functionality to remove number by clicking selected number

--- src/components/Square.js
+++ src/components/Square.js
@@ -2,11 +2,15 @@ import React, { useState } from 'react'
 
 function Square() {
 
-    const [number, setNumber] = useState(0)
+    const [number, setNumber] = useState()
   return (
     <>
     <p>Current Number {number}</p>
     <div className='square'>
+        {number ? 
+        <div className='number'>
+            <h2 onClick={() => setNumber()}>{number}</h2>
+        </div> :
         <div className='numbers'>
             <p onClick={() => setNumber(1)}>1</p>
             <p onClick={() => setNumber(2)}>2</p>
@@ -17,7 +21,7 @@ function Square() {
             <p onClick={() => setNumber(7)}>7</p>
             <p onClick={() => setNumber(8)}>8</p>
             <p onClick={() => setNumber(9)}>9</p>
-        </div>
+        </div>}
     </div>
     </>
   )
