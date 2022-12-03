950231f4652167d6b25ed03936140931f46723e4
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-12-02 20:53:04 -0600

    Create Board

---
+++ src/components/Board.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Section from './Section'
+
+export default function Board() {
+  return (
+    <div className='board'>
+        <Section backgroundColor="blue"/>
+        <Section backgroundColor="white"/>
+        <Section backgroundColor="blue"/>
+        <Section backgroundColor="white"/>
+        <Section backgroundColor="blue"/>
+        <Section backgroundColor="white"/>
+        <Section backgroundColor="blue"/>
+        <Section backgroundColor="white"/>
+        <Section backgroundColor="blue"/>
+        <Section backgroundColor="white"/>
+    </div>
+  )
+}
