2b9fb277631b8da8917888d6ebb995332a6d5c90
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-12-02 20:47:03 -0600

    Render section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import Square from "./components/Square";
+import Section from "./components/Section";
 
 function App() {
   return (
     <div className="App">
       <h1>Sodoku Checker</h1>
-      <Square />
+      <Section />
     </div>
   );
 }
