8d12aa603aa923d2b947a6fa80dff1204e71b471
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-12-02 20:47:15 -0600

    Create section

---
+++ src/components/Section.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Square from './Square'
+
+export default function Section() {
+  return (
+    <div className='section'>
+        <Square />
+        <Square />
+        <Square />
+        <Square />
+        <Square />
+        <Square />
+        <Square />
+        <Square />
+        <Square />
+    </div>
+  )
+}
