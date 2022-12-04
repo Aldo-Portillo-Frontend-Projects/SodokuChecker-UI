821e0a965d8d50caf2c4db6d4e57334039c8b5f2
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-12-04 10:41:40 -0600

    FIx typos

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { sudokuIsValid } from "./algo/main";
 import Board from "./components/Board";
 
 function App() {
-  const sodoku1 = [
+  const [sudoku, setSudoku] = useState([
     [ 8,9,5,   7,4,2,   1,3,6 ],
     [ 2,7,1,   9,6,3,   4,8,5 ],
     [ 4,6,3,   5,8,1,   7,9,2 ],
@@ -14,9 +14,9 @@ function App() {
 
     [ 1,5,9,   8,7,4,   6,2,3 ],
     [ 7,4,6,   3,2,5,   8,1,9 ],
-    [ 3,2,8,   1,9,6,   5,4,7 ]]
+    [ 3,2,8,   1,9,6,   5,4,7 ]])
 
-    const sodoku = [
+    const [sudoku1, setSudoku1] = useState([
       [ 0,0,0,0,0,0,0,0,0 ],
       [ 0,0,0,0,0,0,0,0,0 ],
       [ 0,0,0,0,0,0,0,0,0 ],
@@ -27,14 +27,13 @@ function App() {
   
       [ 0,0,0,0,0,0,0,0,0 ],
       [ 0,0,0,0,0,0,0,0,0 ],
-      [ 0,0,0,0,0,0,0,0,0 ],]
-
-    console.log(sodoku)
+      [ 0,0,0,0,0,0,0,0,0 ],])
+    
   return (
     <div className="App">
-      <h1>Sodoku Checker</h1>
-      <Board sodoku={sodoku}/>
-      <h2>The Game is {sudokuIsValid(sodoku) ? "Complete" : "Incomplete"}</h2>
+      <h1>Sudoku Checker</h1>
+      <Board sudoku={sudoku}/>
+      <h2>The Game is {sudokuIsValid(sudoku) ? "Complete" : "Incomplete"}</h2>
     </div>
   );
 }
