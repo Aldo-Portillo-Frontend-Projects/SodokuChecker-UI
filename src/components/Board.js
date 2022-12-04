8c205bddca9b758d5910f4f957df07aae7077894
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-12-03 21:38:30 -0600

    Fix indexes

--- src/components/Board.js
+++ src/components/Board.js
@@ -4,9 +4,9 @@ import Section from './Section'
 export default function Board({ sodoku}) {
   return (
     <div className='board'>
-        <Section backgroundColor="blue" xmin={0}  ymin={0}  sodoku={sodoku}/>
-        <Section backgroundColor="white"  xmin={0}  ymin={3}  sodoku={sodoku}/>
-        <Section backgroundColor="blue"  xmin={0}  ymin={6}  sodoku={sodoku}/>
+        <Section backgroundColor="white"  xmin={0}  ymin={0}  sodoku={sodoku}/>
+        <Section backgroundColor="blue"  xmin={0}  ymin={3}  sodoku={sodoku}/>
+        <Section backgroundColor="blue" xmin={0}  ymin={6}  sodoku={sodoku}/>
         <Section backgroundColor="white"  xmin={3}  ymin={0}  sodoku={sodoku}/>
         <Section backgroundColor="blue"  xmin={3}  ymin={3}  sodoku={sodoku}/>
         <Section backgroundColor="white"  xmin={3}  ymin={6}  sodoku={sodoku}/>
