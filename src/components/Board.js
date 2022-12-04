41920fb697c0265d1e5e0b5546651f38b9728d4a
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-12-04 10:29:50 -0600

    Fix background colors

--- src/components/Board.js
+++ src/components/Board.js
@@ -4,8 +4,8 @@ import Section from './Section'
 export default function Board({ sodoku}) {
   return (
     <div className='board'>
-        <Section backgroundColor="white"  xmin={0}  ymin={0}  sodoku={sodoku}/>
-        <Section backgroundColor="blue"  xmin={0}  ymin={3}  sodoku={sodoku}/>
+        <Section backgroundColor="blue"  xmin={0}  ymin={0}  sodoku={sodoku}/>
+        <Section backgroundColor="white"  xmin={0}  ymin={3}  sodoku={sodoku}/>
         <Section backgroundColor="blue" xmin={0}  ymin={6}  sodoku={sodoku}/>
         <Section backgroundColor="white"  xmin={3}  ymin={0}  sodoku={sodoku}/>
         <Section backgroundColor="blue"  xmin={3}  ymin={3}  sodoku={sodoku}/>
