41ac8d1de98f4373cb665d31e65e94ab233b56d8
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-12-04 12:21:26 -0600

    Correct typos

--- src/components/Section.js
+++ src/components/Section.js
@@ -6,9 +6,9 @@ export default function Section({backgroundColor, xmin,ymin,sudoku, setSudoku}) 
     <div className={`section ${backgroundColor}`}>
         <Square setSudoku={setSudoku} x={xmin} y={ymin} sudoku={sudoku}/>
         <Square setSudoku={setSudoku} x={xmin} y={ymin + 1} sudoku={sudoku}/>
-        <Square setSudoku={setSudoku} x={xmin} y={ymin + 2} sodoku={sudoku}/>
-        <Square setSudoku={setSudoku} x={xmin +1} y={ymin} sodoku={sudoku}/>
-        <Square setSudoku={setSudoku} x={xmin +1} y={ymin + 1} sodoku={sudoku}/>
+        <Square setSudoku={setSudoku} x={xmin} y={ymin + 2} sudoku={sudoku}/> 
+        <Square setSudoku={setSudoku} x={xmin +1} y={ymin} sudoku={sudoku}/>
+        <Square setSudoku={setSudoku} x={xmin +1} y={ymin + 1} sudoku={sudoku}/>
         <Square setSudoku={setSudoku} x={xmin +1} y={ymin + 2} sudoku={sudoku}/>
         <Square setSudoku={setSudoku} x={xmin +2} y={ymin} sudoku={sudoku}/>
         <Square setSudoku={setSudoku} x={xmin +2} y={ymin + 1} sudoku={sudoku}/>
