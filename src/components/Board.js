bb42c657a12e7c59eb4990fe6a27a8809e6fc627
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-12-04 10:31:52 -0600

    Passdown setSudoku and fix typos

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,18 +1,18 @@
 import React from 'react'
 import Section from './Section'
 
-export default function Board({ sodoku}) {
+export default function Board({ setSudoku, sudoku}) {
   return (
     <div className='board'>
-        <Section backgroundColor="blue"  xmin={0}  ymin={0}  sodoku={sodoku}/>
-        <Section backgroundColor="white"  xmin={0}  ymin={3}  sodoku={sodoku}/>
-        <Section backgroundColor="blue" xmin={0}  ymin={6}  sodoku={sodoku}/>
-        <Section backgroundColor="white"  xmin={3}  ymin={0}  sodoku={sodoku}/>
-        <Section backgroundColor="blue"  xmin={3}  ymin={3}  sodoku={sodoku}/>
-        <Section backgroundColor="white"  xmin={3}  ymin={6}  sodoku={sodoku}/>
-        <Section backgroundColor="blue"  xmin={6}  ymin={0}  sodoku={sodoku}/>
-        <Section backgroundColor="white"  xmin={6}  ymin={3}  sodoku={sodoku}/>
-        <Section backgroundColor="blue"  xmin={6}  ymin={6}  sodoku={sodoku}/>
+        <Section setSudoku={setSudoku} backgroundColor="blue"  xmin={0}  ymin={0}  sudoku={sudoku}/>
+        <Section setSudoku={setSudoku} backgroundColor="white"  xmin={0}  ymin={3}  sudoku={sudoku}/>
+        <Section setSudoku={setSudoku} backgroundColor="blue" xmin={0}  ymin={6}  sudoku={sudoku}/>
+        <Section setSudoku={setSudoku} backgroundColor="white"  xmin={3}  ymin={0}  sudoku={sudoku}/>
+        <Section setSudoku={setSudoku} backgroundColor="blue"  xmin={3}  ymin={3}  sudoku={sudoku}/>
+        <Section setSudoku={setSudoku} backgroundColor="white"  xmin={3}  ymin={6}  sudoku={sudoku}/>
+        <Section setSudoku={setSudoku} backgroundColor="blue"  xmin={6}  ymin={0}  sudoku={sudoku}/>
+        <Section setSudoku={setSudoku} backgroundColor="white"  xmin={6}  ymin={3}  sudoku={sudoku}/>
+        <Section setSudoku={setSudoku} backgroundColor="blue"  xmin={6}  ymin={6}  sudoku={sudoku}/>
     </div>
   )
 }
